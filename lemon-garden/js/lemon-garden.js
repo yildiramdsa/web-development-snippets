04ac078e43bbb73dfef66819d56871236beeaf0b
Alina Yildir <ms.alina.yildir@gmail.com>
2024-11-21 22:05:42 -0700

    lemon-garden project

---
+++ lemon-garden/js/lemon-garden.js
@@ -0,0 +1,53 @@
+jQuery(document).ready(function ($) {
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 57
+  });
+  $("#karatas-fidan-logo").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+  $("#navbarResponsive a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+  $("#navbar-footer a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+  $("#read-more").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+});
